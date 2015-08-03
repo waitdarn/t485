f4a4cfc3128a99328f5e34d0ef7539fbbd21e142
richyliu <richy.liu.2002@gmail.com>
2015-08-03 13:21:39 -0700

    update js game

--- game-assets/game.js
+++ game-assets/game.js
@@ -1,13 +1,11 @@
-// Define stage
-var stage;
-
-// Variables
-var CENTER_X = 240;
-var CENTER_Y = 170;
+// Constants
+var CENTER_X = 550 / 2;
+var CENTER_Y = 300 / 2;
 
 function main() {
-    // Link canvas and enable mouse
-    stage = new createjs.Stage('game');
+    // Link canvas and background and enable mouse
+    var stage = new createjs.Stage('game');
+    var image = new createjs.Bitmap('game-assets/bg.png');
     //stage.mouseEventsEnabled = true;
 
     var circle = new createjs.Shape();
@@ -16,62 +14,12 @@ function main() {
     circle.y = CENTER_Y - 25;
     circle.addEventListener('click', function (e) {
         createjs.Tween.get(circle).to({y: circle.y + 10}, 400);
-        console.log('clicked');
+        console.log('clicked circle');
     });
 
+    stage.addChild(image);
     stage.addChild(circle);
 
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener('tick', stage);
-/*
-    // Load images
-    bgSrc.src = 'game-assets/bg.png';
-    bgSrc.name = 'bg';
-    bgSrc.onload = loadGfx;
-
-    // Ticker
-    createjs.Ticker.setFPS(30);
-    createjs.Ticker.addEventListener('tick', stage);
-
-    console.log('end of main');*/
-}
-
-function loadImage() {
-    var preload = new createjs.LoadQueue();
-    preload.addEventListener('fileload', function(e) {
-        document.body.appendChild(e.result);
-    });
-    preload.loadFile('game-assets/bg.png');
-}
-
-function buildInterface() {
-    btn.x = CENTER_X - 64;
-    btn.y = CENTER_Y - 64;
-
-    stage.addChild(bg, btn);
-
-    // Add buttoon listener
-    btn.onPress = showText;
-    console.log('built interface');
-}
-
-function showText() {
-    console.log('this works');
-
-    // Remove listener
-    btn.onPress = null;
-
-    // Create text
-    var msg = new Text('Hello World!', 'Bold 25px; Arial', '#000');
-
-    msg.x = CENTER_X - 70;
-    msg.y = CENTER_Y;
-
-    stage.addChild(msg);
-    msg.alpha = 0;
-
-    // Animation
-    createjs.Tween.get(btn).todo({y: CENTER_Y + 50}, 300);
-    createjs.Tween.get(msg).wait(400).to({alpha: 1}, 400);
-    console.log('finished showing text');
 }
