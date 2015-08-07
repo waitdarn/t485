cb3497f80e3ca744c46aa3cb8665705e1a00bced
richyliu <richy.liu.2002@gmail.com>
2015-08-07 15:21:55 -0700

    update js game

--- game-assets/game.js
+++ game-assets/game.js
@@ -1,12 +1,3 @@
-/*
-  _|_|_|            _|    _|      _|            _|  _|            _|
-    _|    _|_|_|        _|_|_|_|        _|_|_|  _|      _|_|_|_|      _|_|_|      _|_|_|
-    _|    _|    _|  _|    _|      _|  _|    _|  _|  _|      _|    _|  _|    _|  _|    _|
-    _|    _|    _|  _|    _|      _|  _|    _|  _|  _|    _|      _|  _|    _|  _|    _|
-  _|_|_|  _|    _|  _|      _|_|  _|    _|_|_|  _|  _|  _|_|_|_|  _|  _|    _|    _|_|_|
-                                                                                      _|
-                                                                                  _|_|
-*/
 /*(function () {/()*/
 
     // Init some useful stuff for easier access (don't need them all)
@@ -37,107 +28,163 @@
     })();
 
     var SCALE,
-        WIDTH,
-        HEIGHT,
         canvas,
         ctx,
         world,
         fixDef,
-        shapes = {};
+        shapes = {},
+        images = [];
 
     var debug = false;
-
+/*
+  _|_|_|            _|    _|      _|            _|  _|            _|
+    _|    _|_|_|        _|_|_|_|        _|_|_|  _|      _|_|_|_|      _|_|_|      _|_|_|
+    _|    _|    _|  _|    _|      _|  _|    _|  _|  _|      _|    _|  _|    _|  _|    _|
+    _|    _|    _|  _|    _|      _|  _|    _|  _|  _|    _|      _|  _|    _|  _|    _|
+  _|_|_|  _|    _|  _|      _|_|  _|    _|_|_|  _|  _|  _|_|_|_|  _|  _|    _|    _|_|_|
+                                                                                      _|
+                                                                                  _|_|
+*/
     var init = {
         start: function(id) {
             this.defaultProperties();
             this.canvas(id);
+            this.loadImages();
 
             box2d.create.world();
             box2d.create.defaultFixture();
 
-            this.surroundings.leftWall();
-            this.surroundings.rightWall();
-            this.surroundings.ground();
-            this.surroundings.post();
-
+            this.loadStaticElements();
             this.callbacks();
 
-            setTimeout(function() { add.box({width: 2, height: 2, x: 5, y: 0}); }, 0);
-            //setTimeout(function() { add.random(); }, 100);
-            //setTimeout(function() { add.random(); }, 500);
-            //setTimeout(function() { add.random(); }, 700);
-            //setTimeout(function() { add.random(); }, 1000);
+            //setTimeout(function() { add.box({width: 2, height: 2, x: 5, y: 1, color: 'turquoise'}); }, 0);
+            //setTimeout(function() { add.circle({x: 5, y: 0, radius: 2}); }, 300);
+            //setTimeout(function() { add.triangle({x1: 3, y1: 1, x2: 5, y2: 3, x3: 3, y3: 3}); }, 500);
 
-            // On my signal: Unleash hell.
-            (function hell() {
+            // Game loop
+            (function gameLoop() {
                 loop.step();
                 loop.update();
                 if (debug) {
                     world.DrawDebugData();
                 }
                 loop.draw();
-                requestAnimFrame(hell);
+                requestAnimFrame(gameLoop);
             })();
         },
         defaultProperties: function() {
             SCALE = 30;
-            WIDTH = 900;
-            HEIGHT = 540;
         },
         canvas: function(id) {
             canvas = document.getElementById(id);
             ctx = canvas.getContext("2d");
         },
-        surroundings: {
-            post: function() {
-                add.box({
-                    x: WIDTH / SCALE / 2,
-                    y: HEIGHT / SCALE / 2,
-                    height: 6,
-                    width: 2,
-                    isStatic: true
-                });
-            },
-            rightWall: function() {
-                add.box({
-                    x: WIDTH / SCALE + 1.1,        // 740 / 30 + 1.1
-                    y: HEIGHT / SCALE / 2,        // 380px / 30 / 2
-                    height: HEIGHT / SCALE,   // 380px / 30
-                    width: 2,
-                    isStatic: true
-                });
-            },
-            ground: function() {
-                add.box({
-                    x: WIDTH / SCALE / 2,        // 740 / 30 / 2
-                    y: HEIGHT / SCALE + 1.1,
-                    height: 2,
-                    width: WIDTH / SCALE,     // 740 / 30
-                    isStatic: true
-                });
-            },
-            leftWall: function() {
-                add.box({
-                    x: -1,
-                    y: HEIGHT / SCALE / 2,        // 380px / 30 / 2
-                    height: HEIGHT / SCALE,   // 380px / 30
-                    width: 2,
-                    isStatic: true
-                });
-            }
+        loadImages: function() {
+            var imageObj = new Image();
+            imageObj.src = 'game-assets/grid.gif';
+            images[0] = imageObj;
+            console.log('image(s) loaded');
+        },
+/*
+    _|_|_|    _|                  _|      _|                  _|_|_|  _|
+  _|        _|_|_|_|    _|_|_|  _|_|_|_|        _|_|_|      _|        _|_|_|      _|_|_|  _|_|_|      _|_|      _|_|_|
+    _|_|      _|      _|    _|    _|      _|  _|              _|_|    _|    _|  _|    _|  _|    _|  _|_|_|_|  _|_|
+        _|    _|      _|    _|    _|      _|  _|                  _|  _|    _|  _|    _|  _|    _|  _|            _|_|
+  _|_|_|        _|_|    _|_|_|      _|_|  _|    _|_|_|      _|_|_|    _|    _|    _|_|_|  _|_|_|      _|_|_|  _|_|_|
+                                                                                          _|
+                                                                                          _|
+*/
+        loadStaticElements: function() {
+            // Max x: 30; max y: 18
+            add.box({
+                x: 10,
+                y: 10,
+                height: 1,
+                width: 20,
+                color: 'skyblue',
+                isStatic: true,
+                angle: 0.05
+            });
+            add.box({
+                x: 20,
+                y: 5,
+                height: 1,
+                width: 20,
+                color: 'darkblue',
+                isStatic: true
+            });
+            add.box({
+                x: 26.5,
+                y: 13,
+                height: 1,
+                width: 7,
+                color: 'pink',
+                isStatic: true,
+                angle: -0.6
+            });
+            add.box({
+                x: 2.5,
+                y: 7,
+                height: 1,
+                width: 5,
+                color: 'gold',
+                isStatic: true,
+                angle: 0.6
+            });
+            // Right wall
+            add.box({
+                x: canvas.width / SCALE + 1.1,        // 740 / 30 + 1.1
+                y: canvas.height / SCALE / 2,        // 380px / 30 / 2
+                height: canvas.height / SCALE,   // 380px / 30
+                width: 2,
+                isStatic: true
+            });
+            // Floor
+            add.box({
+                x: canvas.width / SCALE / 2,        // 740 / 30 / 2
+                y: canvas.height / SCALE + 1.1,
+                height: 2,
+                width: canvas.width / SCALE,     // 740 / 30
+                isStatic: true,
+                angle: -0.04,                    // number of rotations
+                color: 'purple'
+            });
+            // Left wall
+            add.box({
+                x: -1,
+                y: canvas.height / SCALE / 2,        // 380px / 30 / 2
+                height: canvas.height / SCALE,   // 380px / 30
+                width: 2,
+                isStatic: true
+            });
         },
         callbacks: function() {
             canvas.addEventListener('click', function(e) {
+                // Adds circle on click
                 var shapeOptions = {
                     x: (canvas.width / SCALE) * (e.offsetX / canvas.width),
-                    y: 0
+                    y: 1,
+                    width: 2,
+                    height: 2,
+                    radius: 1
                 };
-                add.random(shapeOptions);
+                //add.random(shapeOptions);
+                add.circle(shapeOptions);
+                //add.box(shapeOptions);
+                console.log('clicked');
             }, false);
         }
     };
+/*
 
-
+    _|_|          _|        _|        _|_|_|  _|                                                    _|_|_|_|
+  _|    _|    _|_|_|    _|_|_|      _|        _|_|_|      _|_|_|  _|_|_|      _|_|      _|_|_|      _|        _|    _|  _|_|_|      _|_|_|
+  _|_|_|_|  _|    _|  _|    _|        _|_|    _|    _|  _|    _|  _|    _|  _|_|_|_|  _|_|          _|_|_|    _|    _|  _|    _|  _|
+  _|    _|  _|    _|  _|    _|            _|  _|    _|  _|    _|  _|    _|  _|            _|_|      _|        _|    _|  _|    _|  _|
+  _|    _|    _|_|_|    _|_|_|      _|_|_|    _|    _|    _|_|_|  _|_|_|      _|_|_|  _|_|_|        _|          _|_|_|  _|    _|    _|_|_|
+                                                                  _|
+                                                                  _|
+*/
     var add = {
         random: function(options) {
             options = options || {};
@@ -148,7 +195,7 @@
             }
         },
         circle: function(options) {
-            options.radius = 0.5 + Math.random()*1;
+            options.radius = options.radius || 0.5 + Math.random()*1;
             var shape = new Circle(options);
             shapes[shape.id] = shape;
             box2d.addToWorld(shape);
@@ -227,8 +274,15 @@
             }
         }
     };
-
-
+/*
+  _|
+  _|          _|_|      _|_|    _|_|_|
+  _|        _|    _|  _|    _|  _|    _|
+  _|        _|    _|  _|    _|  _|    _|
+  _|_|_|_|    _|_|      _|_|    _|_|_|
+                                _|
+                                _|
+*/
     var loop = {
         step: function() {
             var stepRate = 1 / 60;
@@ -236,15 +290,16 @@
             world.ClearForces();
         },
         update: function () {
+            if (!debug) ctx.clearRect(0, 0, canvas.width, canvas.height);
+
             for (var b = world.GetBodyList(); b; b = b.m_next) {
                 if (b.IsActive() && typeof b.GetUserData() !== 'undefined' && b.GetUserData() != null) {
+                    ctx.drawImage(images[0], 0, 0);
                     shapes[b.GetUserData()].update(box2d.get.bodySpec(b));
                 }
             }
         },
         draw: function() {
-            if (!debug) ctx.clearRect(0, 0, canvas.width, canvas.height);
-
             for (var i in shapes) {
                 shapes[i].draw();
             }
@@ -274,12 +329,10 @@ _|_|_|    _|    _|    _|_|_|  _|_|_|      _|_|_|  _|_|_|
 
     var Shape = function(v) {
         this.id = Math.round(Math.random() * 1000000);
-        //this.x = v.x || Math.random()*23 + 1;
-        this.x = v.x /*|| Math.random()*23 + 1*/;
-        this.y = v.y /*|| 0*/;
-        this.angle = 0;
-        //this.color = helpers.randomColor();
-        this.color = '#33CC00';
+        this.x = v.x || v.x1;
+        this.y = v.y || v.y1;
+        this.angle = v.angle || 0;
+        this.color =  v.color || helpers.randomColor();
         this.center = { x: null, y: null };
         this.isStatic = v.isStatic || false;
 
@@ -300,7 +353,6 @@ _|_|_|    _|    _|    _|_|_|  _|_|_|      _|_|_|  _|_|_|
             ctx.translate(this.x * SCALE, this.y * SCALE);
             ctx.rotate(this.angle);
             ctx.translate(-(this.x) * SCALE, -(this.y) * SCALE);
-
             ctx.fillStyle = this.color;
             ctx.beginPath();
             ctx.arc(this.x * SCALE, this.y * SCALE, this.radius * SCALE, 0, Math.PI * 2, true);
