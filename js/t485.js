0745a580abe835228c9cadd4cb58b97e3c75dea6
richyliu <richy.liu.2002@gmail.com>
2015-09-13 16:27:18 -0700

    fix dropdown menu bug

--- js/t485.js
+++ js/t485.js
@@ -1,45 +1,9 @@
 // Back to top button animation
-$(window).scroll(function() { var x = $(this).scrollTop() !== 0 ? $('#toTop').fadeIn(3000) : $('#toTop').fadeOut(); });
+$(window).scroll(function() { var x = $(this).scrollTop() !== null ? $('#toTop').fadeIn(3000) : $('#toTop').fadeOut(); });
 $('#toTop').click(function() { $('body').animate({scrollTop: 0}, 800); });
 
 // Set LESS async to true to prevent warning on Chrome
 less = {async: true};
 
-
-/*var faqopen = false;
-$('#faq-dropdown').click(function () {
-    if (!faqopen) {
-        $(".navbar-nav dropdown #faq-dropdown").parent().addClass('open');
-        faqopen = true;
-    } else {
-        $(".navbar-nav dropdown #faq-dropdown").parent().removeClass('open');
-    }
-});
-
-var resourceopen = false;
-$('#resource-dropdown').click(function () {
-    if (!resourceopen) {
-        $(".navbar-nav dropdown #resource-dropdown").parent().addClass('open');
-        resourceopen = true;
-    } else {
-        $(".navbar-nav dropdown #resource-dropdown").parent().removeClass('open');
-    }
-});
-
-var directoryopen = false;
-$('#directory-dropdown').click(function () {
-    if (!directoryopen) {
-        $(".navbar-nav dropdown #directory-dropdown").parent().addClass('open');
-        directoryopen = true;
-    } else {
-        $(".navbar-nav dropdown #directory-dropdown").parent().removeClass('open');
-    }
-});*/
-
-$('.navbar-nav dropdown a').click(function() {
-    if (!$(this).parent().hasClass('open')) {
-        $(this).parent().addClass('open');
-    } else {
-        $(this).parent().removeClass('open');
-    }
-});
+// Fix dropdown menu bug on iOS
+$('.dropdown a').click(function() { var x = $(this).parent().hasClass('open') ? $(this).removeClass('open') : $(this).addClass('open'); });
