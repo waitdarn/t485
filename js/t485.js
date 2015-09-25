8d38c14043a0cb8a773b564c4b1b949d9c92148d
richyliu <richy.liu.2002@gmail.com>
2015-09-24 21:55:20 -0700

    fix minor bugs

--- js/t485.js
+++ js/t485.js
@@ -1,9 +1,9 @@
 // Back to top button animation
-$(window).scroll(function() { var x = $(this).scrollTop() !== null ? $('#toTop').fadeIn(3000) : $('#toTop').fadeOut(); });
-$('#toTop').click(function() { $('body,html').animate({scrollTop: 0}, 800); });
+$(window).scroll(function(){ x = $(this).scrollTop() > 0 ? $('#toTop').fadeIn(3000) : $('#toTop').fadeOut(); });
+$('#toTop').click(function(){ $('body,html').animate({scrollTop: 0}, 800); });
 
 // Set LESS async to true to prevent warning on Chrome
 less = {async: true};
 
 // Fix dropdown menu bug on iOS
-$('.dropdown a').click(function() { var x = $(this).parent().hasClass('open') ? $(this).parent().removeClass('open') : $(this).parent().addClass('open'); });
+$('.dropdown a').click(function() { x = $(this).parent().hasClass('open') ? $(this).parent().removeClass('open') : $(this).parent().addClass('open'); });
