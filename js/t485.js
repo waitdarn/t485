b99d84f37c7360fe8ef5d06c71d26996ac0c814c
richyliu <richy.liu.2002@gmail.com>
2015-10-25 18:29:52 -0700

    remove img res test

--- js/t485.js
+++ js/t485.js
@@ -1,12 +1,19 @@
 // Back to top button animation
-$(window).scroll(function(){ x = $(this).scrollTop() > 0 ? $('#toTop').fadeIn(3000) : $('#toTop').fadeOut(); });
+$(window).scroll(function(){ if ($(this).scrollTop() > 0) { $('#toTop').fadeIn(3000); } else { $('#toTop').fadeOut(); }});
 $('#toTop').click(function(){ $('body,html').animate({scrollTop: 0}, 800); });
 
 // Set LESS async to true to prevent warning on Chrome
 less = {async: true};
 
 // Fix dropdown menu bug on iOS
-$('.dropdown a').click(function() { x = $(this).parent().hasClass('open') ? $(this).parent().removeClass('open') : $(this).parent().addClass('open'); });
+$('.dropdown a').click(function() {
+    if ($(this).parent().hasClass('open')) {
+        $(this).parent().removeClass('open');
+    } else {
+        $('.dropdown').removeClass('open');
+        $(this).parent().addClass('open');
+    }
+});
 
 function hashPassword(str) {
     var hash = 0;
