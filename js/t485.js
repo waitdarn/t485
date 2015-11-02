f6438329981e76a47eee9ad3a20c438bd9bab6a7
richyliu <richy.liu.2002@gmail.com>
2015-11-01 16:11:01 -0800

    change to top icon

--- js/t485.js
+++ js/t485.js
@@ -1,6 +1,5 @@
 // Back to top button animation
 $(window).scroll(function(){ if ($(this).scrollTop() > 0) { $('#toTop').fadeIn(3000); } else { $('#toTop').fadeOut(); }});
-$('#toTop').click(function(){ $('body,html').animate({scrollTop: 0}, 800); });
 
 // Set LESS async to true to prevent warning on Chrome
 less = {async: true};
