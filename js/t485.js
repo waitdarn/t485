83a1d7756131b8ba58d95c14e7046fc885082512
jeffreymeng <jkmexpress@yahoo.com>
2016-01-02 21:21:37 -0800

    add support for browser session cookies in the setCookie() function

--- js/t485.js
+++ js/t485.js
@@ -31,10 +31,14 @@ function hashPassword(str) {
 }
 
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+    if (exdays === null || exdays === "" || exdays === "browser" || exdays === 0) {
+        document.cookie = cname + "=" + cvalue + "; ";
+    } else {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
 }
 
 function getCookie(cname) {
