7c0d8f59de9406670e826716d75b4342c0ac6d6d
Richard Liu <richy.liu.2002@gmail.com>
2016-01-07 05:05:25 +0000

    obfuscate code

--- js/t485.js
+++ js/t485.js
@@ -65,15 +65,20 @@ function getVarsFromUrl() {
 
 
 // Checks if the user is logged in
-function auth() {
-    return getCookie('dwrtqd4cmx22y6jji3xy') === '6grg2zb0cjknxtsi9dm3';
-}
+eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){
+while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};
+c=1}while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}
+return p}('1 0(){2 3(\'4\')===\'5\'}',6,6,
+'auth|function|return|getCookie|dw|39483970481658019767'.split('|'),0,{}));
 
 
 // Sets the cookie for login
-function setAuth() {
-    document.cookie = 'dwrtqd4cmx22y6jji3xy=6grg2zb0cjknxtsi9dm3';
-}
+eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){
+while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};
+c=1}while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}
+return p}('1 0(){2.3=\'4=5\'}',6,6,
+'setAuth|function|document|cookie|dw|39483970481658019767'.split('|'),0,{}));
+
 
 
 function logout() {
