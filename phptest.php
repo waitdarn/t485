782dfd25f1ee0168fd37841f5716f716bd971f4e
Richard Liu <richy.liu.2002@gmail.com>
2016-01-22 05:17:30 +0000

    critical bug fix

--- phptest.php
+++ phptest.php
@@ -7,24 +7,19 @@
     <link rel="icon" type="image/png" href="favicons/favicon-16x16.png" sizes="16x16">
 
     <title>PHP Test Page</title>
+    
 </head>
 
 <body>
     
-    <p>Testing 123</p>
-    
     <?php
-        $authorized_ips = array("205.174.240.241");
+        $authorized_ips = array("76.247.185.191");
         
         $user_ip = $_SERVER["REMOTE_ADDR"];
-        $authorized = true;
+        $authorized = in_array($user_ip, $authorized_ips);
         
-        foreach ($authorized_ips as $ip) {
-            if ($user_ip == $ip) $authorized = true;
-        }
-        
-        echo $user_ip;
-        echo "Auth: $authorized";
+        echo "<p>$user_ip</p>";
+        echo "<p>Auth: ", $authorized ? 'true' : 'false', "</p>";
     ?>
     
 </body>
