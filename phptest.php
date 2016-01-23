59c9dd6f41b8b4ca9fcfc376c23efcd3be0c54c0
Richard Liu <richy.liu.2002@gmail.com>
2016-01-23 01:18:15 +0000

    bug fix

--- phptest.php
+++ phptest.php
@@ -11,15 +11,41 @@
 </head>
 
 <body>
+    <p style="font-size: 16pt; margin-bottom: 40px;">This page is in development.</p>
     
     <?php
-        $authorized_ips = array("76.247.185.191");
         
-        $user_ip = $_SERVER["REMOTE_ADDR"];
+        // Read authorized ips file
+        $filename = "authorized_ips";
+        
+        $authorized_ips = file($filename, FILE_IGNORE_NEW_LINES);
+        print_r($authorized_ips);
+        
+        
+        // Get user ip
+        // $user_ip = $_SERVER["REMOTE_ADDR"];
+        $user_ip = "10.240.0.157";
         $authorized = in_array($user_ip, $authorized_ips);
         
+        
+        // Debug code
         echo "<p>$user_ip</p>";
         echo "<p>Auth: ", $authorized ? 'true' : 'false', "</p>";
+        
+        
+        // Write ip to file
+        if (!$authorized) {
+            $myfile = fopen($filename, "r+") or die("Unable to open file!");
+            
+            array_push($authorized_ips, $user_ip);
+            print_r($authorized_ips);
+            
+            foreach($authorized_ips as $ip) {
+                fwrite($myfile, $ip . "\n");
+            }
+            
+            fclose($myfile);
+        }
     ?>
     
 </body>
