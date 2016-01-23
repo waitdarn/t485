0a51378a5d108d4482515b2e96dfeb09a40691ab
Richard Liu <richy.liu.2002@gmail.com>
2016-01-23 20:51:35 +0000

    bug fix

--- logout.php
+++ logout.php
@@ -5,22 +5,21 @@
     
     
     // Get user ip
-    $user_ip = $_SERVER["REMOTE_ADDR"];
+    // $user_ip = $_SERVER["REMOTE_ADDR"];
+    $user_ip = '76.247.185.191';
     $authorized = in_array($user_ip, $authorized_ips);
     
     
     // Write ip to file
-    if (!$authorized) {
-        $myfile = fopen($filename, "r+") or die("Unable to open file!");
-        
-        // Removes user ip
-        array_splice($authorized_ips , array_search($user_ip, $authorized_ips), 1);
-        echo $authorized_ips;
-        
-        foreach($authorized_ips as $ip) {
-            fwrite($myfile, $ip . "\n");
-        }
-        
-        fclose($myfile);
+    $myfile = fopen($filename, "r+") or die("Unable to open file!");
+    
+    // Removes user ip
+    array_splice($authorized_ips, array_search($user_ip, $authorized_ips), 1);
+    
+    
+    foreach($authorized_ips as $ip) {
+        fwrite($myfile, $ip . "\n");
     }
+    
+    fclose($myfile);
 ?>
--- setlogin.php
+++ setlogin.php
@@ -9,15 +9,13 @@
     
     
     // Write ip to file
-    if (!$authorized) {
-        $myfile = fopen($filename, "r+") or die("Unable to open file!");
-        
-        array_push($authorized_ips, $user_ip);
-        
-        foreach($authorized_ips as $ip) {
-            fwrite($myfile, $ip . "\n");
-        }
-        
-        fclose($myfile);
+    $myfile = fopen($filename, "r+") or die("Unable to open file!");
+    
+    array_push($authorized_ips, $user_ip);
+    
+    foreach($authorized_ips as $ip) {
+        fwrite($myfile, $ip . "\n");
     }
+    
+    fclose($myfile);
 ?>
