1e379ce8db5d0a0ae9245b40496b2a78472fb5e8
Richard Liu <richy.liu.2002@gmail.com>
2016-01-24 22:56:05 +0000

    remove

--- logout.php
+++ logout.php
@@ -10,13 +10,13 @@
     $authorized = in_array($user_ip, $authorized_ips);
     
     
-    // Write ip to file
+    
     $myfile = fopen($filename, "r+") or die("Unable to open file!");
     
     // Removes user ip
     array_splice($authorized_ips, array_search($user_ip, $authorized_ips), 1);
     
-    
+    // Write ip to file
     foreach($authorized_ips as $ip) {
         fwrite($myfile, $ip . "\n");
     }
