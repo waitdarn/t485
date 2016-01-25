a8f67f6b459cf9ed14b35a9117aec307e934dc3c
Richard Liu <richy.liu.2002@gmail.com>
2016-01-25 04:16:07 +0000

    bug

--- mail.php
+++ mail.php
@@ -1,21 +1,38 @@
 <?php
-    
-    $to = "richy.liu.2002@gmail.com";
-    $subject = "File to add (auto-generated)";
-    $txt = "Url: {$_POST["link"]}\nFile name: {$_POST["name"]}.\nReferrer: {$_POST["from"]}";
-    $headers = "From: {$_POST["email"]}";
-    
-    // $to = "richy.liu.2002@gmail.com";
-    // $subject = "generated test file";
-    // $txt = "test 123";
-    // $headers = "From: testtest";
-    
-    
-    echo "<p>$to</p>";
-    echo "<p>$subject</p>";
-    echo "<p>$txt</p>";
-    echo "<p>$headers</p>";
-    
-    mail($to, $subject, $txt, $headers);
-    
+    $redir = $_POST["from"];
 ?>
+
+<html>
+    
+    <head>
+        <title>Redirecting...</title>
+        <META http-equiv="refresh" content="0;URL=http://t485.org/<?=$redir?>">
+    </head>
+    
+    <body>
+        
+        <?php
+            $to = "richy.liu.2002@gmail.com";
+            $subject = "File to add (auto-generated)";
+            $txt = "Url: {$_POST["link"]}\nFile name: {$_POST["name"]}.\nReferrer: {$_POST["from"]}";
+            $headers = "From: {$_POST["email"]}";
+            
+            // $to = "richy.liu.2002@gmail.com";
+            // $subject = "generated test file";
+            // $txt = "test 123";
+            // $headers = "From: testtest";
+            
+            
+            echo "<p>$to</p>";
+            echo "<p>$subject</p>";
+            echo "<p>$txt</p>";
+            echo "<p>$headers</p>";
+            
+            mail($to, $subject, $txt, $headers);
+        ?>
+        
+    </body>
+    
+</html>
+
+
