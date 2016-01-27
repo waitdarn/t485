5a726ab180c44f7023c1b2f2f9842d064d219a9d
Richard Liu <richy.liu.2002@gmail.com>
2016-01-27 03:28:41 +0000

    critical

--- logout.php
+++ logout.php
@@ -3,14 +3,11 @@
     $filename = "authorized_ips";
     $authorized_ips = file($filename, FILE_IGNORE_NEW_LINES);
     
-    
     // Get user ip
     $user_ip = $_SERVER["REMOTE_ADDR"];
-    // $user_ip = '76.247.185.191';
     
     
-    
-    $myfile = fopen($filename, "r+") or die("Unable to open file!");
+    $myfile = fopen($filename, "w") or die("Unable to open file!");
     
     // Removes user ip
     if(($key = array_search($user_ip, $authorized_ips)) !== false) {
