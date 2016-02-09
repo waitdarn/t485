fb876bda6dc423bc8eccf29bb7ba1d40a1b5c1e0
Richard Liu <richy.liu.2002@gmail.com>
2016-02-09 00:52:10 +0000

    switched to firebase auth remembering

--- logout.php
+++ logout.php
@@ -2,18 +2,37 @@
     // Read authorized ips file
     $filename = "authorized_ips.nocache";
     $authorized_ips_string = fgets(fopen($filename, 'r'));
+    
     // Trims trailing commas
-    rtrim($string, ",");
+    $authorized_ips_string = rtrim($authorized_ips_string, ",");
+    
+    // Turn string into array
     $authorized_ips = explode(",", $authorized_ips_string);
+    foreach ($authorized_ips as $ip) {
+        $authorized_ips = explode("|", $ip);
+    }
     
     
     // Get user ip
     $user_ip = $_POST["fingerprint"];
     
     
+    // Get timestamp
+    $date = date_create();
+    
+    
     // Removes user ip
-    if(($key = array_search($user_ip, $authorized_ips)) !== false) {
-        unset($authorized_ips[$key]);
+    for($i = 0; $i < sizeof($authorized_ips); $i++) {
+        if ($authorized_ips[$i][0] == $user_ip) {
+            unset($authorized_ips[$i]);
+        }
+    }
+    
+    // Remove expired ips
+    for($i = 0; i < sizeof($authorized_ips); $i++) {
+        if ($authorized_ips[$i][1] > $curTime) {
+            unset($authorized_ips[$i]);
+        }
     }
     
     
--- setlogin.php
+++ setlogin.php
@@ -2,29 +2,49 @@
     // Read authorized ips file
     $filename = "authorized_ips.nocache";
     $authorized_ips_string = fgets(fopen($filename, 'r'));
+    
     // Trims trailing commas
-    rtrim($string, ",");
+    $authorized_ips_string = rtrim($authorized_ips_string, ",");
+    
+    // Turn string into array
     $authorized_ips = explode(",", $authorized_ips_string);
+    for($i = 0; $i < sizeof($authorized_ips); $i++) {
+        $authorized_ips[$i] = explode("|", $authorized_ips[$i]);
+    }
+    var_dump($authorized_ips);
+    echo "<p></p>";
     
     
     // Get user ip
     $user_ip = $_POST["fingerprint"];
+    var_dump($user_ip);
+    echo "<p></p>";
     
     
     // Get timestamp
     $date = date_create();
-    $time = date_timestamp_get($date);
-    $time += 86400;
+    $curTime = date_timestamp_get($date);
+    $time = $curTime + 86400;
+    
     
     // Add user ip
-    array_push($authorized_ips, "$user_ip");
+    array_push($authorized_ips, "{$user_ip}|{$time}");
+    
+    // Remove expired ips
+    for($i = 0; $i < sizeof($authorized_ips); $i++) {
+        if ($authorized_ips[$i][1] > $curTime) {
+            unset($authorized_ips[$i]);
+        }
+    }
+    var_dump($authorized_ips);
+    echo "<p></p>";
     
     
-    // Write ip to file
+    // // Write ip to file
     $myfile = fopen($filename, "r+") or die("Unable to open file!");
     
     foreach($authorized_ips as $ip) {
-        fwrite($myfile, $ip . ",");
+        // fwrite($myfile, $ip . ",");
     }
     
     fclose($myfile);
