cd54748078e3a545ffa06249be10ac968f10b720
Richard Liu <richy.liu.2002@gmail.com>
2016-02-12 04:26:31 +0000

    clean up code

--- logout.php
+++ logout.php
@@ -1,3 +1,59 @@
+<?php
+    // Read authorized ips file
+    $response = file_get_contents("https://t485.firebaseio.com/authorized.json");
+    // echo "<p>$response</p>";
+    $response_decoded = json_decode($response);
+    var_dump($response_decoded);
+    echo "<p></p>";
+    
+    $authorized_ips = (Array) $response_decoded;
+    echo "<p></p>";
+    
+    
+    // Get timestamp
+    $date = date_create();
+    
+    // Remove expired ips
+    for($i = 0; $i < sizeof($authorized_ips); $i++) {
+        if ($authorized_ips[$i]->{"expire"} < $curTime) {
+            unset($authorized_ips[$i]);
+        }
+    }
+    var_dump($authorized_ips);
+    echo "<p></p>";
+    
+    
+    // Get redirect url
+    $redir = htmlspecialchars($_GET["redir"]);
+    
+    
+    // Get user ip
+    $user_ip = htmlspecialchars($_GET["fingerprint"]);
+    // $user_ip = "07074660851debcf1fb8127875a32270";
+    var_dump($user_ip);
+    echo "<p></p>";
+    
+    // Removes user ip
+    for($i = 0; $i < sizeof($authorized_ips); $i++) {
+        if ($authorized_ips[$i]->{"fingerprint"} == $user_ip) {
+            unset($authorized_ips[$i]);
+        }
+    }
+    var_dump($authorized_ips);
+    echo "<p></p>";
+    
+    
+    // Turn array into string
+    $str = "";
+    foreach($authorized_ips as $ip) {
+        $str .= "{'expire':";
+        $str .= $ip->{"expire"};
+        $str .= ",'fingerprint':'";
+        $str .= $ip->{"fingerprint"};
+        $str .= "'},";
+    }
+    $str = rtrim($str, ",");
+?>
 <html>
     
     <head>
@@ -6,60 +62,8 @@
     
     <body>
         
-        <p>If you don't get redirected within 5 seconds, click <a href="logout.html">here</a>.</p>
+        <p>If you don't get redirected within 5 seconds, click <a href="<?=$redir?>">here</a>.</p>
         
-        <?php
-            // Read authorized ips file
-            $response = file_get_contents("https://t485.firebaseio.com/authorized.json");
-            // echo "<p>$response</p>";
-            $response_decoded = json_decode($response);
-            var_dump($response_decoded);
-            echo "<p></p>";
-            
-            $authorized_ips = (Array) $response_decoded;
-            echo "<p></p>";
-            
-            
-            // Get timestamp
-            $date = date_create();
-            
-            // Remove expired ips
-            for($i = 0; $i < sizeof($authorized_ips); $i++) {
-                if ($authorized_ips[$i]->{"expire"} < $curTime) {
-                    unset($authorized_ips[$i]);
-                }
-            }
-            var_dump($authorized_ips);
-            echo "<p></p>";
-            
-            
-            // Get user ip
-            $user_ip = htmlspecialchars($_GET["fingerprint"]);
-            // $user_ip = "07074660851debcf1fb8127875a32270";
-            var_dump($user_ip);
-            echo "<p></p>";
-            
-            // Removes user ip
-            for($i = 0; $i < sizeof($authorized_ips); $i++) {
-                if ($authorized_ips[$i]->{"fingerprint"} == $user_ip) {
-                    unset($authorized_ips[$i]);
-                }
-            }
-            var_dump($authorized_ips);
-            echo "<p></p>";
-            
-            
-            // Turn array into string
-            $str = "";
-            foreach($authorized_ips as $ip) {
-                $str .= "{'expire':";
-                $str .= $ip->{"expire"};
-                $str .= ",'fingerprint':'";
-                $str .= $ip->{"fingerprint"};
-                $str .= "'},";
-            }
-            $str = rtrim($str, ",");
-        ?>
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
         <script src="https://cdn.firebase.com/js/client/2.4.0/firebase.js"></script>
@@ -70,7 +74,7 @@
             
             var ref = new Firebase('https://t485.firebaseio.com/authorized');
             ref.set(main, function(error) {
-                window.location.replace('index.html');
+                window.location.replace('<?=$redir?>');
             });
         </script>
     
