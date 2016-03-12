f1516766b51a2f6ef521def4cf118b996b84871a
Richard Liu <richy.liu.2002@gmail.com>
2016-03-12 00:42:34 +0000

    set up multisite

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -79,6 +79,14 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 define( 'WP_ALLOW_MULTISITE', true );
+
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 't485.org');
+define('PATH_CURRENT_SITE', '/wordpress/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
