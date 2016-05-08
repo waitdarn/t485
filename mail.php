594bb3c005443875aba741e74855ac7bac4ffd04
Richard Liu <richy.liu.2002@gmail.com>
2016-05-08 23:49:55 +0000

    remove all php for transition to github pages

--- mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $to = $_POST["recipents"];
-    $subject = $_POST["subject"];
-    $content = $_POST["content"];
-    $headers = "From: " . $_POST["from"];
-    
-    mail($to, $subject, $content, $headers);
-?>
