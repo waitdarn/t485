1257e57770319e96fc350aa6e5087f69e00e629d
Richard Liu <richy.liu.2002@gmail.com>
2016-05-23 18:49:00 -0700

    Hot bug fix enable login

--- js/t485.js
+++ js/t485.js
@@ -129,8 +129,7 @@ function mainAuth(onAuthed, onUnauthed) {
             }
             
             // USE IN EMERGENCY ONLY
-            authed = true;
-            console.log('auth status: ' + authed);
+            // authed = true;
             
             console.log('auth status: ' + authed);
             // Run callbacks
