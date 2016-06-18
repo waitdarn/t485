1daa0ca6a525aadff434b519ff337b01d7a99b42
Richard Liu <richy.liu.2002@gmail.com>
2016-06-18 21:08:36 +0000

    update offline app and bug fix navbar

--- offline/my-app.js
+++ offline/my-app.js
@@ -12,57 +12,83 @@ var mainView = myApp.addView('.view-main', {
 
 
 
-// Now we need to run the code that will be executed only for About page.
 
 var data = [];
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function(e) {
-    // Get page data from event data
-    var page = e.detail.page;
-
-    if (page.name === 'directory') {
-        // Following code will be executed for page with data-page attribute equal to "about"
+myApp.onPageInit('directory', function() {
+    data = JSON.parse(localStorage.getItem('directory-info'));
+    for (var i = 0; i < data.length; i++) {
+        var currentName = data[i]['Scout\'s Full Name (last name first):'];
         
-        
-        data = JSON.parse(localStorage.getItem('directory-info'));
-        console.log(data);
-        
-        
-        for (var i = 0; i < data.length; i++) {
-            $$('#search-content > ul').append(
-                '<li>' +
-                    '<a href="#" class="item-link open-popup" data-popup=".directory-more-popup" onclick="showfullinfo(' + i + ')">' +
-                        '<div class="item-content">' +
-                            '<div class="item-inner">' +
-                                '<div class="item-title">' + data[i]['Scout\'s Full Name (last name first):'] + '</div>' +
-                            '</div>' +
-                        '</div>' +
-                    '</a>' +
-                '</li>'
-            );
+        // if name is in format: last, first --> turn into --> first last
+        if (currentName.indexOf(',') > -1) {
+            if (currentName.indexOf(' ') > -1) {
+                currentName = currentName.split(', ')[1] + ' ' + currentName.split(', ')[0];
+            } else {
+                currentName = currentName.split(',')[1] + ' ' + currentName.split(',')[0];
+            }
         }
-    } else if (page.name === 'resources') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        // myApp.alert('Here comes the resources page');
+        data[i]['Scout\'s Full Name (last name first):'] = currentName;
+    }
+    
+    // sort data according to name
+    data = data.sort(function(a, b) {
+        var key = 'Scout\'s Full Name (last name first):';
+        return a[key] < b[key] ? -1 : 1;
+    });
+    
+    
+    for (var i = 0; i < data.length; i++) {
+        currentName = data[i]['Scout\'s Full Name (last name first):'];
         
-        
+        $$('#search-content > ul').append(
+            '<li>' +
+                '<a href="directory-more.html" class="item-link" onclick="showfullinfo(' + i + ')">' +
+                    '<div class="item-content">' +
+                        '<div class="item-inner">' +
+                            '<div class="item-title">' + currentName + '</div>' +
+                            '<div class="item-after">' + data[i]['patrol'] + '</div>' +
+                        '</div>' +
+                    '</div>' +
+                '</a>' +
+            '</li>'
+        );
     }
 });
 
 
-
 function showfullinfo(id) {
-    console.log(id);
-    $$('#directory-more-popup-title').html(data[id]['Scout\'s Full Name (last name first):']);
-    $$('#directory-more-popup-info').html('').append(
-        'Name: '                    + data[id]['Scout\'s Full Name (last name first):'] + '<br>' +
-        'Email: <a href="mailto:'   + data[id]['Scout\'s E-mail:']                 + '">' + data[id]['Scout\'s E-mail:'] + '</a><br>' +
-        'Cell Phone: <a href="tel:' + data[id]['Scout\'s Cell Phone']              + '">' + data[id]['Scout\'s Cell Phone'] + '</a><br>' +
-        'Home Phone: <a href="tel:' + data[id]['Scout\'s Home Phone']              + '">' + data[id]['Scout\'s Home Phone'] + '</a><br>' +
-        'Patrol: '                  + data[id]['patrol']                           + '<br><br>' +
-        'Father\'s Cell Phone: <a href="tel:' + data[id]['Father\'s Cell Phone']   + '">' + data[id]['Father\'s Cell Phone'] + '</a><br>' +
-        'Father\'s E-mail: <a href="mailto:'  + data[id]['Father\'s E-mail']       + '">' + data[id]['Father\'s E-mail'] + '</a><br>' +
-        'Mother\'s Cell Phone: <a href="tel:' + data[id]['Mother\'s Cell Phone']   + '">' + data[id]['Mother\'s Cell Phone'] + '</a><br>' +
-        'Mother\'s E-mail: <a href="mailto:'  + data[id]['Mother\'s E-mail']       + '">' + data[id]['Mother\'s E-mail'] + '</a><br>'
-    );
+    myApp.onPageInit('directory-more', function() {
+        console.log(id);
+        $$('#more-name').html(data[id]['Scout\'s Full Name (last name first):']);
+        $$('#more-info').html('').append(
+            'Name: '                    + data[id]['Scout\'s Full Name (last name first):'] + '<br>' +
+            'Email: <a href="mailto:'   + data[id]['Scout\'s E-mail:']                 + '">' + data[id]['Scout\'s E-mail:'] + '</a><br>' +
+            'Cell Phone: <a href="tel:' + data[id]['Scout\'s Cell Phone']              + '">' + data[id]['Scout\'s Cell Phone'] + '</a><br>' +
+            'Home Phone: <a href="tel:' + data[id]['Scout\'s Home Phone']              + '">' + data[id]['Scout\'s Home Phone'] + '</a><br>' +
+            'Patrol: '                  + data[id]['patrol']                           + '<br><br>' +
+            'Father\'s Cell Phone: <a href="tel:' + data[id]['Father\'s Cell Phone']   + '">' + data[id]['Father\'s Cell Phone'] + '</a><br>' +
+            'Father\'s E-mail: <a href="mailto:'  + data[id]['Father\'s E-mail']       + '">' + data[id]['Father\'s E-mail'] + '</a><br>' +
+            'Mother\'s Cell Phone: <a href="tel:' + data[id]['Mother\'s Cell Phone']   + '">' + data[id]['Mother\'s Cell Phone'] + '</a><br>' +
+            'Mother\'s E-mail: <a href="mailto:'  + data[id]['Mother\'s E-mail']       + '">' + data[id]['Mother\'s E-mail'] + '</a><br>'
+        );
+    });
 }
+
+
+
+myApp.onPageInit('resources', function() {
+    /*
+    <li>
+        <a href="#" class="item-link">
+            <div class="item-content">
+                <div class="item-inner">
+                    <div class="item-title">test link</div>
+                    <div class="item-after">3 MB</div>
+                </div>
+            </div>
+        </a>
+    </li>
+    */
+    
+    
+});
